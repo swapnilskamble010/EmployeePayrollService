212eb03fd5c1587f10658c7a84cfeafe72b59def
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-30 20:47:05 +0530

    Welcome message added

---
+++ EmployeePayroll.java
@@ -0,0 +1,7 @@
+package employeepayrollservice;
+
+public class EmployeePayroll {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll Service Program");
+	}
+}
